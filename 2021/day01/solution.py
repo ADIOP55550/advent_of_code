1e445423edecc7eb73d0e7675939187be6640086
ADIOP55550 <klamutadrian382@gmail.com>
2022-11-02 17:17:07 +0100

    day 1 part 2
    Signed-off-by: ADIOP55550 <klamutadrian382@gmail.com>

--- 2021/day01/solution.py
+++ 2021/day01/solution.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import deque
+
+
 def main():
     with open("input.txt") as f:
         lines = f.readlines()
 
-        prev = None
+        prev = deque()
+        curr = deque()
         totalInc = 0
+        firstCurr = True
+        c = None
 
         for line in lines:
-            curr = int(line)
-            if prev == None:
-                prev = curr
+            c = int(line)
+            if len(prev) < 3:
+                prev.append(c)
                 continue
 
-            if prev < curr:
+            if firstCurr:
+                firstCurr = False
+                curr = prev.copy()
+                curr.popleft()
+                curr.append(c)
+            
+            if sum(prev) < sum(curr):
                 totalInc += 1
 
-            prev = curr
+            prev.popleft()
+            prev.append(curr[2])
+            curr.popleft()
+            curr.append(c)
+
 
         print(totalInc)
 
